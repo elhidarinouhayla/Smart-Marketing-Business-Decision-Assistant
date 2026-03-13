4c1acfeb2260fe0c5cd7fc41459beb4f333f8bf7
Nouhayla <elhidarinouhayla@gmail.com>
2026-03-13 14:51:12 +0000

    Update campaign schema

--- backend/app/schemas/campaign.py
+++ backend/app/schemas/campaign.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel, ConfigDict
 from uuid import UUID
 from enum import Enum
-from typing import Optional, List
 
 class CampaignStatusEnum(str, Enum):
     DRAFT = "draft"
